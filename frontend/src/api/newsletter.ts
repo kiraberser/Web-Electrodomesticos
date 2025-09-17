8f05983969df400c134fc52a1c1d078b0f0a66c0
kiraberser <121148745+kiraberser@users.noreply.github.com>
2025-09-16 21:35:06 -0600

    Post email by API

---
+++ frontend/src/api/newsletter.ts
@@ -0,0 +1,21 @@
+'use server'
+
+import axios from 'axios'
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL_API
+
+export const subscribeNewsletter = async (email: string) => {
+    const url = `${BASE_URL}/common/newsletters/`
+    try {
+        const res = await axios.post(url, { email }, {
+            headers: { 'Content-Type': 'application/json' },
+        })
+        return { success: true, status: res.status, data: res.data }
+    } catch (error: any) {
+        const status = error?.response?.status || 500
+        const data = error?.response?.data || { message: 'Error inesperado' }
+        return { success: false, status, data }
+    }
+}
+
+
