8916881945140bf4ca03cac81ac02c0a73d34cdb
kiraberser <121148745+kiraberser@users.noreply.github.com>
2025-09-22 15:38:55 -0600

    middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { cookies } from 'next/headers';
 
 function isAdminFromJWT(jwt?: string): boolean {
     if (!jwt) return false;
